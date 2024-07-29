e32fb1e06b476a7ec0f06a0430a8177c719d82da
venkataranjit <112625318+venkataranjit@users.noreply.github.com>
2024-07-29 11:43:33 +0530

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+  document.write(
+"<div class='footer'>"+
+  "<div class='container'>"+
+      "<div class='col-xs-12 col-sm-6'>"+
+  "<p>All copy rights reserved &copy 2020</p></div>"+
+      "<div class='col-xs-12 col-sm-6' style='text-align:right; color:#fff'>Site best viewed in Chrome, Mozilla / Firefox, Opera, IE9+</div>"+
+  "</div>"+
+  "</div>"
+    );
---
+++ js/header.js
@@ -0,0 +1,96 @@
+  document.write(
+"<audio src='ohm.mp3' controls='' autoplay='' loop='' style='position: absolute;visibility: hidden;'></audio>"+
+"<img src='images/garuda.png' style='position: fixed;left: 2px;bottom: -13px;z-index: 9999;'>"+
+"<img src='images/hanuma.png' style='position: fixed;right: -2px;bottom: -13px;z-index: 9999;'>"+ 
+  "<div class='row topbg'></div>"+
+   "<div class='row header_row'>"+
+        "<div class='container' style='text-align:center'>"+
+        "<img src='images/logo1.png' class='logo'style='max-width:100%' />"+
+        "</div>"+
+    "</div>"+
+"<div class='row' style='margin: 0;background: #fff;background: #fdaf00;'>"+
+    "<nav class='navbar navbar-default'>"+
+        "<div class='' style='padding-left:0px;'>"+
+        "<div class='container-fluid'>"+
+          "<div class='navbar-header'>"+
+            "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>"+
+              "<span class='sr-only'>Toggle navigation</span>"+
+              "<span class='icon-bar'></span>"+
+              "<span class='icon-bar'></span>"+
+              "<span class='icon-bar'></span>"+
+            "</button>"+            
+          "</div>"+
+          "<div id='navbar' class='navbar-collapse collapse'>"+
+            "<ul class='nav navbar-nav'>"+
+              "<li class=''><a href='#'>Home</a></li>" +
+               "<li class='dropdown'>"+
+              "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>About Temple <b class='caret'></b></a>"+
+              "<ul class='dropdown-menu'>"+
+                "<li><a href='#'>History</a></li>" +
+                "<li><a href='#'>Temple List</a></li>" +
+                "<li><a href='#'>Arjita Sevas</a></li>" +
+              "</ul>"+
+            "</li>"+
+              "<li class='dropdown'>"+
+              "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Utsavas <b class='caret'></b></a>"+
+              "<ul class='dropdown-menu'>"+
+                "<li><a href='#'>Utsavams in the Temple</a></li>" +
+                "<li><a href='#'>Nrusimha Jayanti</a></li>" +
+                "<li><a href='#'>Kartika Pournami</a></li>" +                
+              "</ul>"+
+            "</li>"+
+              "<li class='dropdown'>"+
+              "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Godavari <b class='caret'></b></a>"+
+              "<ul class='dropdown-menu'>"+
+                "<li><a href='#'>Dakshina Vahini</a></li>" +
+              "</ul>"+
+            "</li>"+
+                 "<li><a href='#'>Excutive Body</a></li>" +              
+                   "<li class='dropdown'>"+
+              "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Pilgrims Info <b class='caret'></b></a>"+
+              "<ul class='dropdown-menu'>"+
+                "<li><a href='#'>How to Reach</a></li>" +
+                "<li><a href='#'>Accommodation</a></li>" +
+                "<li><a href='#'>Appeal to Devotees</a></li>" +
+                 "<li><a href='#'>Travelling Facilities </a></li>" +
+                  "<li><a href='#'>Facilities to Piligrims</a></li>" +                  
+              "</ul>"+
+            "</li>"+
+               "<li class='dropdown'>"+
+              "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Other Info <b class='caret'></b></a>"+
+              "<ul class='dropdown-menu'>"+
+                "<li><a href='#'>Narasimha Shatakam</a></li>" +
+                "<li><a href='#'>Stotra Malika</a></li>" +
+                "<li><a href='#'>Photo Gallary</a></li>" +
+                 "<li><a href='#'>Sri Matham</a></li>" +
+                "<li><a href='#'  target='_blank'>Astrology</a></li>" +
+                "<li><a href='#' target='_blank'>Panchanga</a></li>" +
+                 "<li><a href='#'>Legends</a></li>" +              
+              "</ul>"+
+            "</li>" +
+
+
+
+
+
+                  "<li><a href='#'>Downloads</a></li>" +
+
+
+
+                   "<li class='dropdown'>" +
+              "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Contact Us <b class='caret'></b></a>" +
+              "<ul class='dropdown-menu'>" +
+                "<li><a href='#'>Temple Contact Info</a></li>" +
+                "<li><a href='#'>Temple Staff Phone Numbers</a></li>" +
+               
+              "</ul>" +
+            "</li>" +
+                           
+                
+            "</ul>"+
+          "</div>"+
+        "</div>"+
+            "</div>"+
+      "</nav>"+
+	  "</div>"
+    );
